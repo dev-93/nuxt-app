ef8e5c26ac6d4c36ba5e61b8358801f5f440c2c2
kimtaenam <cjb940103@gmail.com>
2022-05-08 16:41:45 +0900

    strict 검사 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-})
+  typescript: {
+    strict: true,
+  },
+});
